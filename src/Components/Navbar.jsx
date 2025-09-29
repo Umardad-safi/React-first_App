cd458f5d185aba0ef87fb959df14c09524c1b79b
https://github.com/Umardad-safi <udad0171@gmail.com>
2025-09-29 16:14:35 +0430

    update the Navbar component that remove the duplicate code from my code

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,27 +1,36 @@
 import React, { useState } from "react";
 import { close, logo, menu } from "../assets/images";
 import { navLinks } from "../Constants/index";
+
 export default function Navbar() {
   const [active, setActive] = useState("Home");
   const [toggle, setToggle] = useState(false);
+
+  const renderNavLinks = (isMobile = false) =>
+    navLinks.map((nav, index) => (
+      <li
+        key={nav.id}
+        className={`font-normal font-poppins text-[16px] cursor-pointer ${
+          active === nav.title ? "text-white" : " text-dimWhite"
+        }
+         ${
+           index === navLinks.length - 1 ? "mr-0" : isMobile ? "mb-2" : "mr-10"
+         }`}
+        onClick={() => {
+          setActive(nav.title);
+          if (isMobile) setToggle(false);
+        }}>
+        <a href={`${nav.id}`}> {nav.title}</a>
+      </li>
+    ));
 
   return (
     <nav className="w-full py-6 flex justify-between items-center">
       <img src={logo} alt="" className="w-[124px] h-[32px]" />
 
+      {/* Desktop menu */}
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
-        {navLinks.map((nav, index) => {
-          return (
-            <li
-              key={nav.id}
-              className={`font-normal font-poppins text-[16px] cursor-pointer ${
-                active === nav.title ? "text-white" : "text-dimWhite"
-              } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
-              onClick={() => setActive(nav.title)}>
-              <a href={`#${nav.id}`}>{nav.title}</a>
-            </li>
-          );
-        })}
+        {renderNavLinks()}
       </ul>
 
       {/* Mobile menu */}
@@ -37,21 +46,9 @@ export default function Navbar() {
         <div
           className={`${
             toggle ? "scale-y-100" : "scale-y-0"
-          } p-6 bg-black-gradient absolute top-20 left-0 mx-2  min-w-[140px] rounded-b-xl sidebar z-50  transform transition-transform duration-500  origin-top  `}>
+          } p-6 bg-black-gradient absolute top-20 left-0 my-1  mx-2  min-w-[140px] rounded-xl sidebar z-50  transform transition-transform duration-500  origin-top  `}>
           <ul className="list-none flex-col justify-end flex-1 items-center">
-            {navLinks.map((nav, index) => {
-              return (
-                <li
-                  className={`${
-                    index === navLinks.length - 1 ? "mr-0" : "mr-10"
-                  } ${active === nav.title ? "text-white " : "text-dimWhite"}`}
-                  onClick={() => {
-                    setActive(nav.title);
-                  }}>
-                  <a href={`#${nav.id}`}>{nav.title}</a>
-                </li>
-              );
-            })}
+            {renderNavLinks(true)}
           </ul>
         </div>
       </div>
